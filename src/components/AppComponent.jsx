14b53dfa37a6276533b5ea78bb3066d18d0fdec9
allen wes <allen.wes.g@gmail.com>
2016-06-03 01:49:49 -0500

    initial files

---
+++ src/components/AppComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../../sass/_hey-guys.scss"; 
+
+class App extends React.Component {
+	render() {
+		return (
+			<h1>Hey Guys!</h1>
+		);
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/AppComponent.jsx";
+
+const mainContainer = document.getElementById("app");
+
+ReactDOM.render(<App/>, mainContainer);
